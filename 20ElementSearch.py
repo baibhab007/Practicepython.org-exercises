612b315072873dedd48638e0b6062e1fb0cf9170
baibhab007 <41581003+baibhab007@users.noreply.github.com>
2019-09-03 20:48:31 +0200

    Create 20ElementSearch.py

---
+++ 20ElementSearch.py
@@ -0,0 +1,8 @@
+def function():
+    list = [1,2,3,4,5,6,7,8,9,10]
+    element = int(input("Enter a number to search in the list: "))
+    if element in list:
+        print("True")
+    else:
+        print("False")
+function()
